873e621b73ec0f5a5f97687b3366aabca08b2488
Sav Younts <savyounts@gmail.com>
2018-08-15 23:17:47 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -19,7 +19,7 @@ class Computer < Player
   def move(board)
     @board = board
      
-        ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+        ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample if win == nil
       
   end  
     #first turn choose corner
