de04754dec5ecf893cfb961ed3f6e975529c345c
Sav Younts <savyounts@gmail.com>
2018-08-15 22:34:30 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -23,7 +23,7 @@ class Game
   
   #state of game methods
    def current_player
-    board.turn_count % 2 == 0 ? @player_1 : @player_2
+    board.turn_count % 2 == 0 ? "X" : "O"
   end 
   
   def won?
