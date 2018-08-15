270e6f6b5a40eeeb1147a2f76f8d45a4faa89f92
Sav Younts <savyounts@gmail.com>
2018-08-15 23:29:51 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -22,8 +22,6 @@ class Computer < Player
        if block == nil
          if create_fork == nil
             ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
-          
-          end 
         end 
       end 
     end
