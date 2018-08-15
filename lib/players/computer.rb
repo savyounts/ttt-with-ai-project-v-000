1ccf3a4a2664d7225864f58b413f58aa89750514
Sav Younts <savyounts@gmail.com>
2018-08-15 20:19:41 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,10 @@ class Computer < Player
   
   def move(board)
     ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
-    if 
+    #if its the first turn choose middle 
+    #if not second turn, choose middle or corner
+    #if 
+    if turn_count
   end
 end 
 end
