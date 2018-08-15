bf1eb54a4d54f424eba1e99d86ae2d97b4d35df6
Sav Younts <savyounts@gmail.com>
2018-08-15 23:27:17 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -21,17 +21,13 @@ class Computer < Player
      if win == nil
        if block == nil
          if create_fork == nil
-          ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+           if defend_fork == nil
+            ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+          end 
         end 
       end 
     end
   end  
-    #first turn choose corner
-    #second turn choose other corner
-    #Win: If you have two in a row, play the third to get three in a row.
-    #Block: If the opponent has two in a row, play the third to block them.
-    #Fork: Create an opportunity where you can win in two ways.
-    #Block Opponent's Fork:
     
     def win
       WIN_COMBINATIONS.detect do |combo|
@@ -74,18 +70,17 @@ class Computer < Player
     end
     
     def defend_fork
-      case opponent_forks
-        when (board.cells[0] != token || board.cells[0] != " ") && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
+        if (board.cells[0] != token || board.cells[0] != " ") && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
           "4"
-        when (board.cells[8] != token || board.cells[0] != " ") && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  
+        elsif (board.cells[8] != token || board.cells[0] != " ") && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  
           "4"
-        when (board.cells[0] != token || board.cells[0] != " ") && board.cells[0] == board.cells[4] 
+        elsif (board.cells[0] != token || board.cells[0] != " ") && board.cells[0] == board.cells[4] 
           ["2", "6"].sample
-        when (board.cells[8] != token || board.cells[0] != " ") && board.cells[8] == board.cells[4]  
+        elsif (board.cells[8] != token || board.cells[0] != " ") && board.cells[8] == board.cells[4]  
           ["2", "6"].sample
-        when (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[2]
+        elsif (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[2]
           ["0", "8"].sample
-        when (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[6]
+        elsif (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[6]
           ["0", "8"].sample
       end
     end
