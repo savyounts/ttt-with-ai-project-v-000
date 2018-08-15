4abb9a870d1577254cc437bdadf40f5f142d7ecb
Sav Younts <savyounts@gmail.com>
2018-08-15 23:44:23 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,7 +18,7 @@ class Computer < Player
   def move(board)
     if @board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       "5"
-     elsif 
+     else 
       ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
     end
   end  
