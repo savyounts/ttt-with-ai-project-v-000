ecf56952aa810a13d317bb3cb305e3ed98728216
Sav Younts <savyounts@gmail.com>
2018-08-15 21:32:33 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,10 @@ module Players
 class Computer < Player
   
   def move(board)
-    ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
+    win
+    block 
+    fork 
+    defend_fork 
   end   
     #first turn choose corner
     #second turn choose other corner
@@ -37,22 +40,32 @@ class Computer < Player
     
     def create_fork 
       case forks
-        when board.cells[0] == player.token && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6] || board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])
+        when board.cells[0] == player.token && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
            "4"
-        when board.cells[0] == player.token && (board.cells[0] == board.cells[4] ||board.cells[8] == board.cells[4]
+        when board.cells[8] == player.token && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  "4"
+        when board.cells[0] == player.token && board.cells[0] == board.cells[4] 
           ["2", "6"].sample
-        when board.cells[2] == player.token && (board.cells[4] == board.cells[2] || board.cells[4] == board.cells[6])
+        when board.cells[8] == player.token && board.cells[8] == board.cells[4]  
+          ["2", "6"].sample
+        when board.cells[2] == player.token && board.cells[4] == board.cells[2]
+          ["0", "8"].sample
+        when board.cells[4] == player.token && board.cells[4] == board.cells[6]
           ["0", "8"].sample
       end
     end
     
     def defend_fork
       case opponent_forks
-        when (board.cells[0] != player.token || board.cells[0] != " ")&& (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6] || board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])
+        when (board.cells[0] != player.token || board.cells[0] != " ") && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
            "4"
-        when board.cells[0] == player.token && (board.cells[0] == board.cells[4] ||board.cells[8] == board.cells[4]
+        when (board.cells[8] != player.token || board.cells[0] != " ") && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  "4"
+        when (board.cells[0] != player.token || board.cells[0] != " ") && board.cells[0] == board.cells[4] 
           ["2", "6"].sample
-        when board.cells[2] == player.token && (board.cells[4] == board.cells[2] || board.cells[4] == board.cells[6])
+        when (board.cells[8] != player.token || board.cells[0] != " ") && board.cells[8] == board.cells[4]  
+          ["2", "6"].sample
+        when (board.cells[4] != player.token || board.cells[0] != " ") && board.cells[4] == board.cells[2]
+          ["0", "8"].sample
+        when (board.cells[4] != player.token || board.cells[0] != " ") && board.cells[4] == board.cells[6]
           ["0", "8"].sample
       end
     end
