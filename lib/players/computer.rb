7355d67f25cc64f243a5599b9132b7bb101c0a8b
Sav Younts <savyounts@gmail.com>
2018-08-15 21:43:08 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,6 +2,17 @@ require 'pry'
 
 module Players
 class Computer < Player
+  
+   WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
   def move(board)
     win || block || create_fork || defend_fork 
   end   
