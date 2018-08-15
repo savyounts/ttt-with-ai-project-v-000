c39310e1f797e86b4d77f2e8dc661fa8ee57c826
Sav Younts <savyounts@gmail.com>
2018-08-15 21:23:14 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -37,8 +37,13 @@ class Computer < Player
     
     def create_fork 
       case 
-        when board.cells[
-
+        when board.cells[0] == player.token && (board.cells[0] == board.cells[2]
+           "4"
+        when board.cells[0] == player.token && (board.cells[0] == board.cells[4] ||board.cells[8] == board.cells[4]
+          ["2", "6"].sample
+        when board.cells[2] == player.token && (board.cells[4] == board.cells[2] || board.cells[4] == board.cells[6])
+          ["0", "8"]
+  
 
 end
 end
