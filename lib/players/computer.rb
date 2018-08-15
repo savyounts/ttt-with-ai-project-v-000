eefe8f3aac7660088d5cdb51a4db9c415b418915
Sav Younts <savyounts@gmail.com>
2018-08-15 23:43:22 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -18,66 +18,67 @@ class Computer < Player
   def move(board)
     if @board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
       "5"
-     elsif win == nil
+    if @board ==  
+     elsif 
       ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
     end
   end  
     
-    def win
-      WIN_COMBINATIONS.detect do |combo|
-        if board.cells[combo[0]] == token && board.cells[combo[0]] == board.cells[combo[1]] 
-            combo[2]
-        elsif board.cells[combo[0]] == token && board.cells[combo[0]] == board.cells[combo[2]]
-            combo[1]
-        elsif board.cells[combo[2]] == token && board.cells[combo[2]] == board.cells[combo[1]]
-            combo[0]
-        end
-      end
-    end 
+    # def win
+    #   WIN_COMBINATIONS.detect do |combo|
+    #     if board.cells[combo[0]] == token && board.cells[combo[0]] == board.cells[combo[1]] 
+    #         combo[2]
+    #     elsif board.cells[combo[0]] == token && board.cells[combo[0]] == board.cells[combo[2]]
+    #         combo[1]
+    #     elsif board.cells[combo[2]] == token && board.cells[combo[2]] == board.cells[combo[1]]
+    #         combo[0]
+    #     end
+    #   end
+    # end 
 
-    def block 
-      WIN_COMBINATIONS.detect do |combo|
-        if board.cells[combo[0]] != token && board.cells[combo[0]] != " " &&  board.cells[combo[0]] == board.cells[combo[1]] 
-            combo[2]
-        elsif board.cells[combo[0]] != token && board.cells[combo[0]] != " " && board.cells[combo[0]] == board.cells[combo[2]]
-            combo[1]
-        elsif board.cells[combo[2]] != token && board.cells[combo[2]] != " " && board.cells[combo[2]] == board.cells[combo[1]]
-            combo[0]
-        end
-      end
-    end
+    # def block 
+    #   WIN_COMBINATIONS.detect do |combo|
+    #     if board.cells[combo[0]] != token && board.cells[combo[0]] != " " &&  board.cells[combo[0]] == board.cells[combo[1]] 
+    #         combo[2]
+    #     elsif board.cells[combo[0]] != token && board.cells[combo[0]] != " " && board.cells[combo[0]] == board.cells[combo[2]]
+    #         combo[1]
+    #     elsif board.cells[combo[2]] != token && board.cells[combo[2]] != " " && board.cells[combo[2]] == board.cells[combo[1]]
+    #         combo[0]
+    #     end
+    #   end
+    # end
     
-    def create_fork 
-        if board.cells[0] == token && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
-          "4"
-        elsif board.cells[8] == token && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  
-          "4"
-        elsif board.cells[0] == token && board.cells[0] == board.cells[4] 
-          ["2", "6"].sample
-        elsif board.cells[8] == token && board.cells[8] == board.cells[4]  
-          ["2", "6"].sample
-        elsif board.cells[2] == token && board.cells[4] == board.cells[2]
-          ["0", "8"].sample
-        elsif board.cells[4] == token && board.cells[4] == board.cells[6]
-          ["0", "8"].sample
-      end
-    end
+    # def create_fork 
+    #     if board.cells[0] == token && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
+    #       "4"
+    #     elsif board.cells[8] == token && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  
+    #       "4"
+    #     elsif board.cells[0] == token && board.cells[0] == board.cells[4] 
+    #       ["2", "6"].sample
+    #     elsif board.cells[8] == token && board.cells[8] == board.cells[4]  
+    #       ["2", "6"].sample
+    #     elsif board.cells[2] == token && board.cells[4] == board.cells[2]
+    #       ["0", "8"].sample
+    #     elsif board.cells[4] == token && board.cells[4] == board.cells[6]
+    #       ["0", "8"].sample
+    #   end
+    # end
     
-    def defend_fork
-        if (board.cells[0] != token || board.cells[0] != " ") && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
-          "4"
-        elsif (board.cells[8] != token || board.cells[0] != " ") && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  
-          "4"
-        elsif (board.cells[0] != token || board.cells[0] != " ") && board.cells[0] == board.cells[4] 
-          ["2", "6"].sample
-        elsif (board.cells[8] != token || board.cells[0] != " ") && board.cells[8] == board.cells[4]  
-          ["2", "6"].sample
-        elsif (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[2]
-          ["0", "8"].sample
-        elsif (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[6]
-          ["0", "8"].sample
-      end
-    end
+    # def defend_fork
+    #     if (board.cells[0] != token || board.cells[0] != " ") && (board.cells[0] == board.cells[2] || board.cells[0] == board.cells[6])
+    #       "4"
+    #     elsif (board.cells[8] != token || board.cells[0] != " ") && (board.cells[8] == board.cells[6] || board.cells[8] == board.cells[2])  
+    #       "4"
+    #     elsif (board.cells[0] != token || board.cells[0] != " ") && board.cells[0] == board.cells[4] 
+    #       ["2", "6"].sample
+    #     elsif (board.cells[8] != token || board.cells[0] != " ") && board.cells[8] == board.cells[4]  
+    #       ["2", "6"].sample
+    #     elsif (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[2]
+    #       ["0", "8"].sample
+    #     elsif (board.cells[4] != token || board.cells[0] != " ") && board.cells[4] == board.cells[6]
+    #       ["0", "8"].sample
+    #   end
+    # end
   
 
 end
