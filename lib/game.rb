7ba67e6249428504ef9aaec1b78407191cfafad0
Sav Younts <savyounts@gmail.com>
2018-08-15 19:34:49 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -29,7 +29,6 @@ class Game
   def won?
     WIN_COMBINATIONS.detect do |combo| 
       board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.taken?(combo[0] + 1) 
-      binding.pry
     end
   end 
   
