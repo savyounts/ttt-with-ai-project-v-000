7b1529f08b94e0e2c14982d8c4e621751d894ce4
Sav Younts <savyounts@gmail.com>
2018-08-16 00:25:28 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -26,7 +26,7 @@ class Computer < Player
     
     def win
       WIN_COMBINATIONS.detect do |combo|
-        if board.cells[combo[0]] == token && board.cells[combo[0]] == board.cells[combo[1]] 
+        if board[combo[0]] == token && board[combo[0]] == board[combo[1]] 
             combo[2]
         elsif board.cells[combo[0]] == token && board.cells[combo[0]] == board.cells[combo[2]]
             combo[1]
