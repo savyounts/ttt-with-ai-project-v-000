75dcaec967d93590aac5a3a189a79fdc11e30cf7
Sav Younts <savyounts@gmail.com>
2018-08-16 00:13:36 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -16,12 +16,12 @@ class Computer < Player
   ]
   
   def move(board)
-    if self.game.board.cells == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-      "5"
+    # if self.game.board.cells == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #   "5"
       
-    else 
+    # else 
       ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
-    end
+    # end
   end  
     
     # def win
